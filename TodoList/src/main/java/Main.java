47bb75dda839fdcfd527c66760fb9a61ff89c6cd
g-yala <geroayala@yahoo.com>
2021-06-15 17:16:49 -0700

    adding this into my repo

---
+++ TodoList/src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        public static void main(String[] main) {
+            TodoLIst list = new TodoLIst();
+            list.add("read the course material");
+            list.add("watch the latest fool us");
+            list.add("take it easy");
+            list.print();
+            list.remove(2);
+            list.print();
+            list.add("buy raisins");
+            list.print();
+            list.remove(1);
+            list.remove(1);
+            list.print();
+        }
+    }
+
+}
---
+++ TodoList/src/main/java/ScannerUserInterface.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ScannerUserInterface {
+    private TodoLIst todoList;
+    private Scanner scanner;
+
+    public ScannerUserInterface(TodoLIst todoList, Scanner scanner) {
+        this.todoList = todoList;
+        this.scanner = scanner;
+    }
+    public void start() {
+
+    }
+}
