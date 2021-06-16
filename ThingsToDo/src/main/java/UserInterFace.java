af482aad6788125752364c21a35003c9e142731b
Geronimo Ayala <geronimoa@Geronimos-MacBook-Pro.local>
2021-06-15 17:11:24 -0700

    adding

---
+++ ThingsToDo/src/main/java/UserInterFace.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class UserInterFace {
+    private TodoList todoList;
+    private Scanner scanner;
+
+    public UserInterFace(TodoList todoList, Scanner scanner) {
+        this.todoList = todoList;
+        this.scanner = scanner;
+    }
+    public void start() {
+
+    }
+}
