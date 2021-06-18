9ac0fa2210c2a07d808afb9c37da03cf63ce9b4f
g-yala <geroayala@yahoo.com>
2021-06-17 17:29:04 -0700

    making a todoList

--- TodoList/src/main/java/Main.java
+++ TodoList/src/main/java/Main.java
@@ -1,19 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        public static void main(String[] main) {
-            TodoLIst list = new TodoLIst();
-            list.add("read the course material");
-            list.add("watch the latest fool us");
-            list.add("take it easy");
-            list.print();
-            list.remove(2);
-            list.print();
-            list.add("buy raisins");
-            list.print();
-            list.remove(1);
-            list.remove(1);
-            list.print();
-        }
+    public static void main(String[] main) {
+        TodoLIst list = new TodoLIst();
+        list.add("read the course material");
+        list.add("watch the latest fool us");
+        list.add("take it easy");
+
+        list.print();
+        list.remove(2);
+
+        System.out.println();
+        list.print();
     }
 
 }
